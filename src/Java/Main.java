71701a70044e1ca2d972ea800733cf0c5f9acf8b
Jack Sawyer <jack.h.sawyer@pm.me>
2022-05-16 12:59:51 -0700

    wonderful

--- src/Java/Main.java
+++ src/Java/Main.java
@@ -2,7 +2,7 @@ package Java;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello, World!");
+
+        System.out.println("Hello, WONDERFUL World!");
     }
 }
