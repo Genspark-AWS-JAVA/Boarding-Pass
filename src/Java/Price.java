ba8d2de232695d113429debc64a23c05ce282dd3
imanub5 <imanub5@gmail.comgit config user.email imanub5@gmail.com>
2022-05-18 14:24:53 -0500

    second one

--- src/Java/Price.java
+++ src/Java/Price.java
@@ -1,5 +1,18 @@
 package Java;
 
 public class Price {
-    Float basePrice;
+    int age = 21;
+    String gender = "male";
+    float basePrice = 300f;
+    public Price(User user, BoardingPass boardingPass) {
+    }
+    public float getPrice(){
+        if (gender .equals("female")){
+            basePrice-=basePrice * 0.25;
+        }
+        if( age <= 12 ){
+            basePrice-=basePrice * 0.50;
+        }
+        return basePrice;
+    }
 }
